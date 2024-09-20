a1d6fefe555172fe6847600d5109928e3704f5a4
Rizki Fiananda <rizki@gmail.com>
2024-09-20 14:40:18 +0700

    commit

--- websiteKelas/settings.py
+++ websiteKelas/settings.py
@@ -117,11 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
 LOGIN_URL = ''
 
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # settings.py
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
